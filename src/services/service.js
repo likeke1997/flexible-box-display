7eef92821b9eb782bab1e4b21c6d00188e6d680b
likeke1997 <ke.xiaolong@qq.com>
2020-03-19 09:28:51 +0800

    + 更新接口。

--- src/services/service.js
+++ src/services/service.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default async url => {
   try {
     let res = await axios({
+      method: "POST",
       baseURL: process.env.VUE_APP_APIROOT,
       url
     });
